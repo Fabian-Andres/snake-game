6617bda9a7296b7b56ce9ad8efd6ff42a83a1484
Fabian Riascos <f4bian.andres@gmail.com>
2020-11-18 16:03:46 -0500

    Update assets and eat function

--- web-app/src/game/scenes/gamescene.js
+++ web-app/src/game/scenes/gamescene.js
@@ -1,4 +1,7 @@
 import Phaser from 'phaser';
+import foodImg from '@/assets/game/food.svg';
+import snakeHead from '@/assets/game/snake-head.svg';
+import snakeBody from '@/assets/game/snake.svg';
 
 // Keys controls
 const UP = 0;
@@ -14,20 +17,49 @@ export default class GameScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('body', '../../assets/game/snake.png');
+    this.load.image('food', foodImg);
+    this.load.image('head', snakeHead);
+    this.load.image('body', snakeBody);
   }
 
   create() {
+    const Food = new Phaser.Class({
+      Extends: Phaser.GameObjects.Image,
+      initialize:
+      function Food(scene, x, y) {
+        Phaser.GameObjects.Image.call(this, scene);
+
+        this.setTexture('food');
+        this.setDisplaySize(20, 20);
+        this.setPosition(x * 16, y * 16);
+        this.setOrigin(0);
+
+        this.total = 0;
+
+        scene.children.add(this);
+      },
+      eat() {
+        this.total += 1;
+
+        const x = Phaser.Math.Between(0, 39);
+        const y = Phaser.Math.Between(0, 29);
+
+        this.setPosition(x * 16, y * 16);
+      },
+    });
+
     const Snake = new Phaser.Class({
       initialize:
       function Snake(scene, x, y) {
         this.headPosition = new Phaser.Geom.Point(x, y);
         this.body = scene.add.group();
-        this.head = this.body.create(x * 16, y * 16, 'body');
+        this.head = this.body.create(x * 16, y * 16, 'head');
+        this.head.setDisplaySize(20, 20);
         this.head.setOrigin(0);
         this.alive = true;
         this.speed = 100;
         this.moveTime = 0;
+        this.tail = new Phaser.Geom.Point(x, y);
         this.heading = RIGHT;
         this.direction = RIGHT;
       },
@@ -89,6 +121,7 @@ export default class GameScene extends Phaser.Scene {
           this.body.getChildren(),
           this.headPosition.x * 16,
           this.headPosition.y * 16, 1,
+          this.tail,
         );
 
         // Update the timer ready for the next movement
@@ -97,8 +130,23 @@ export default class GameScene extends Phaser.Scene {
         return true;
       },
 
+      grow() {
+        const newPart = this.body.create(this.tail.x, this.tail.y, 'body');
+        newPart.setDisplaySize(20, 20);
+        newPart.setOrigin(0);
+      },
+
+      collideWithFood(food) {
+        if (this.head.x === food.x && this.head.y === food.y) {
+          this.grow();
+          food.eat();
+          return true;
+        }
+        return false;
+      },
     });
 
+    this.food = new Food(this, 3, 4);
     this.snake = new Snake(this, 8, 8);
 
     // Init keyboard controls
@@ -121,6 +169,8 @@ export default class GameScene extends Phaser.Scene {
       this.snake.faceDown();
     }
 
-    this.snake.update(time);
+    if (this.snake.update(time)) {
+      this.snake.collideWithFood(this.food);
+    }
   }
 }
